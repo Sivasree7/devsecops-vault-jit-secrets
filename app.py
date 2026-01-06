983bddc44ec70a4dcfe55e549cb52798bb30c559
Sivasree Sekar <sivasreesekar2006@gmail.com>
2026-01-06 08:31:43 +0000

    Initial commit: dummy app with Vault secret injection

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+import sys
+secret =os.getenv("DB_PASSWORD")
+if not secret:
+    print("secret not found")
+    sys.exit(1)
+print("secret received securely")
+print("App executed successfully")
